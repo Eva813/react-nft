ef3591aab5a10d4f7c2317fb4e4f02a47256a352
Eva813 <as45986@gmail.com>
2021-12-12 22:38:42 +0800

    modify search-bar icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,32 @@
 import React from "react";
+import './Header.css'
+import punklogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+import themeSwitchIcon from '../assets/header/theme-switch.png'
 
 const Header = () => {
-  return <h2>I am a header :roket</h2>
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punklogo} className="punkLogo" alt=""></img>
+      </div>
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} />
+        </div>
+        <input className="searchInput" placeholder="Search..." />
+      </div>
+      <div className="headerItems">
+        <p>Drop</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitchIcon} alt="themeSwitchIcon"></img>
+        </div>
+      </div>
+    </div>);
 }
 
 export default Header;
